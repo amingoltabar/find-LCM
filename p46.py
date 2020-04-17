9c0a82d45a331caced921ef9230fc4f0fefdc89d
amingoltabar <61889677+amingoltabar@users.noreply.github.com>
2020-04-17 19:40:20 +0430

    Add files via upload

--- p46.py
+++ p46.py
@@ -1,18 +1,37 @@
 my_list=[]
-for i in range(7):#creating the list
-    a=float(input('Create your list.Enter the integer: '))
-    while a!=int(a):
-        a=float(input('You did not enter an integer.Enter the integer: '))
-    a=int(a)
-    my_list.append(a)
-x=float(input('Enter the  index: '))
-while x<0 or x>6 or x!=int(x):#Entered indexes must be integer and in the range of the list.
-    x=float(input('Index you have entered is out of range or non-integer.Enter the index again: '))
-x=int(x)
-y=float(input('Enter the  index: '))
-while y<0 or y>6 or y!=int(y):
-    y=float(input('Index you have entered is out of range or non-integer.Enter the index again: '))
-y=int(y)
+count=1
+print('Creating the list')
+while len(my_list)<7: #creating the list
+    print('Enter the integer number',count,end='')
+    a=input(':')
+    try:
+        val=int(a)
+        my_list.append(val)
+        count+=1
+    except ValueError:
+        try:
+            val=float(a)
+            print('You entered a float')
+        except ValueError:
+            print('You did not enter a number. ')
+index_list=[]
+count_1=0
+while len(index_list)<2: #inputting indexes
+    print('Enter the index number',count_1+1,end='')
+    b=input(':')
+    try:
+        val=int(b)
+        if val<=6 and val>=0:
+            index_list.append(val)
+            count_1+=1
+        else:
+            print('Your entered number is not in range.')
+    except ValueError:
+        try:
+            val=float(b)
+            print('You entered a float.')
+        except ValueError:
+            print('You did not enter a number.')
 def findLCM(x,y,array):#This function receives 2 indexes and calculates the LCM of their elements.
     i=1
     while True:
@@ -21,6 +40,8 @@ def findLCM(x,y,array):#This function receives 2 indexes and calculates the LCM 
             break
         i+=1
     return LCM
+x=index_list[0]
+y=index_list[1]
 answer=findLCM(x,y,my_list)
 print('LCM of index',x,'and index',y,'elements :',answer)
 
