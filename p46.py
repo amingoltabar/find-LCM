85ef0660173e90a540e67310b4402b64b517d657
amingoltabar <61889677+amingoltabar@users.noreply.github.com>
2020-04-22 14:55:01 +0430

    Add files via upload

--- p46.py
+++ p46.py
@@ -1,37 +1,43 @@
+def receive():#Function for inputting integer
+    count=0
+    while count==0:
+        a=input('Enter the integer :')
+        try:
+            val=int(a)
+            count+=1
+            return val
+        except ValueError:
+            try:
+                val=float(a)
+                print('You entered a float. ')
+            except ValueError:
+                print('You did not enter a number. ')
 my_list=[]
 count=1
 print('Creating the list')
 while len(my_list)<7: #creating the list
-    print('Enter the integer number',count,end='')
-    a=input(':')
-    try:
-        val=int(a)
-        my_list.append(val)
-        count+=1
-    except ValueError:
+    my_list.append(receive())
+def receiveindex():#Function for inputting indexe
+    count=0
+    while count==0:
+        a=input('Enter the index :')
         try:
-            val=float(a)
-            print('You entered a float')
+            val=int(a)
+            if val>=0 and val<=6:
+                count+=1
+                return val 
+            else:
+                print('Your entered index is not in range.')
         except ValueError:
-            print('You did not enter a number. ')
+            try:
+                val=float(a)
+                print('You entered a float. ')
+            except ValueError:
+                print('You did not enter a number. ')
 index_list=[]
-count_1=0
-while len(index_list)<2: #inputting indexes
-    print('Enter the index number',count_1+1,end='')
-    b=input(':')
-    try:
-        val=int(b)
-        if val<=6 and val>=0:
-            index_list.append(val)
-            count_1+=1
-        else:
-            print('Your entered number is not in range.')
-    except ValueError:
-        try:
-            val=float(b)
-            print('You entered a float.')
-        except ValueError:
-            print('You did not enter a number.')
+count_1=1
+while len(index_list)<2: 
+    index_list.append(receiveindex())
 def findLCM(x,y,array):#This function receives 2 indexes and calculates the LCM of their elements.
     i=1
     while True:
